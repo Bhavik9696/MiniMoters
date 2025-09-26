a4a638f3567098a71e29d9c2e81d4ef940ab1179
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-09-26 05:35:31 +0530

    create component and connect to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import ProductGrid from './components/ProductGrid';
+import SearchBar from './components/SearchBar';
+import BuyNowModal from './components/BuyNowModal';
+import CustomizeGift from './components/CustomizeGift';
+
 
 function App() {
   
 
   return (
     <>
+    <SearchBar/>
+    <ProductGrid/>
+    <BuyNowModal/>
+    <BuyNowModal/>
+    <CustomizeGift/>
      
     </>
   )
---
+++ src/components/CustomizeGift.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomizeGift = () => {
+  return (
+    <div>
+      CustomizeGift
+    </div>
+  )
+}
+
+export default CustomizeGift
